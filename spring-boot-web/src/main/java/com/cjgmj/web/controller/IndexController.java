76071ced1c7642cddb97286cda24419f8fb0f7d8
cjgmj <cjgmj@hotmail.com>
2019-07-07 15:13:39 +0200

    RequestMapping

--- spring-boot-web/src/main/java/com/cjgmj/web/controller/IndexController.java
+++ spring-boot-web/src/main/java/com/cjgmj/web/controller/IndexController.java
@@ -1,17 +1,18 @@
 package com.cjgmj.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({ "/index", "/", "/home" })
-	public ModelAndView index(ModelAndView mv) {
-		mv.addObject("titulo", "Hola Spring Framework");
-		mv.setViewName("index");
-		return mv;
+	@GetMapping({ "/index", "/", "", "/home" })
+	public String index(Model model) {
+		model.addAttribute("titulo", "Hola Spring Framework");
+		return "index";
 	}
 
 }
