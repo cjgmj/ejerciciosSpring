cfd2e2ff9493ac0bfde879ac8ff8c9322292ee16
cjgmj <cjgmj@hotmail.com>
2019-07-08 18:36:04 +0200

    Directiva if de Thymeleaf

--- spring-boot-web/src/main/java/com/cjgmj/web/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/cjgmj/web/controllers/IndexController.java
@@ -1,9 +1,13 @@
 package com.cjgmj.web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cjgmj.web.models.Usuario;
 
@@ -22,10 +26,19 @@ public class IndexController {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Prueba nombre");
 		usuario.setApellido("Prueba apellido");
+		usuario.setEmail("prueba@prueba.com");
 
 		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
 		model.addAttribute("usuario", usuario);
 		return "perfil";
 	}
 
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+
+		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
+	}
 }
