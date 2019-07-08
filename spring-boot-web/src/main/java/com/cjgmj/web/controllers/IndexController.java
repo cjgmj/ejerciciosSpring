2ecd1e78e92562679d5a281097233feadd8afef7
cjgmj <cjgmj@hotmail.com>
2019-07-08 18:24:19 +0200

    Modelo usuario

--- spring-boot-web/src/main/java/com/cjgmj/web/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/cjgmj/web/controllers/IndexController.java
@@ -1,9 +1,11 @@
-package com.cjgmj.web.controller;
+package com.cjgmj.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cjgmj.web.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
@@ -13,6 +15,17 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring Framework");
 		return "index";
+	}
+
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Prueba nombre");
+		usuario.setApellido("Prueba apellido");
+
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("usuario", usuario);
+		return "perfil";
 	}
 
 }
