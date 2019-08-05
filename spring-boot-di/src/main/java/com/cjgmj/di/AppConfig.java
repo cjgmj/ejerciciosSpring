b8cbb7e6cb9fb652c95ec3e83ac424c90825584f
cjgmj <cjgmj@hotmail.com>
2019-08-05 17:36:59 +0200

    Componente ItemsFactura

--- spring-boot-di/src/main/java/com/cjgmj/di/AppConfig.java
+++ spring-boot-di/src/main/java/com/cjgmj/di/AppConfig.java
@@ -1,9 +1,14 @@
 package com.cjgmj.di;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.cjgmj.di.domain.ItemFactura;
+import com.cjgmj.di.domain.Producto;
 import com.cjgmj.di.services.IndexService;
 import com.cjgmj.di.services.impl.IndexServiceComplejoImpl;
 import com.cjgmj.di.services.impl.IndexServiceImpl;
@@ -20,6 +25,17 @@ public class AppConfig {
 	@Primary
 	public IndexService registrarMiSericioComplejo() {
 		return new IndexServiceComplejoImpl();
+	}
+
+	@Bean
+	public List<ItemFactura> registrarItems() {
+		Producto producto1 = new Producto("Camara Sony", 100.0);
+		Producto producto2 = new Producto("Bicicleta Bianchi aro 26", 200.0);
+
+		ItemFactura item1 = new ItemFactura(producto1, 2);
+		ItemFactura item2 = new ItemFactura(producto2, 4);
+
+		return Arrays.asList(item1, item2);
 	}
 
 }
