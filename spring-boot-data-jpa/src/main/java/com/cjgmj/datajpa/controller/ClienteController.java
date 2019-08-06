1de598a820fd4dc8f09be2149577c09c0e19d3b5
cjgmj <cjgmj@hotmail.com>
2019-08-06 17:25:36 +0200

    Método crear

--- spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/controller/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/controller/ClienteController.java
@@ -1,5 +1,7 @@
 package com.cjgmj.datajpa.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cjgmj.datajpa.dao.IClienteDao;
+import com.cjgmj.datajpa.entity.Cliente;
 
 @Controller
 public class ClienteController {
@@ -21,6 +24,20 @@ public class ClienteController {
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
+	}
+
+	@RequestMapping(value = "/form")
+	public String crear(Map<String, Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("titulo", "Formulario de Cliente");
+		model.put("cliente", cliente);
+		return "form";
+	}
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
 	}
 
 }
