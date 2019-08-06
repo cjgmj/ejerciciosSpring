a65d014bc91776a69dba17852b1deced55bcbad0
cjgmj <cjgmj@hotmail.com>
2019-08-06 17:03:03 +0200

    Crear vista

---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Joe', 'Simons', 'joe.simons@gmail.com', '2019-08-06');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'John', 'Doe', 'john.doe@gmail.com', '2019-08-06');
