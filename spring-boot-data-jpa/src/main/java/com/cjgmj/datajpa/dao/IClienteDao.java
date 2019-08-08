037d2b84535441d26913b213b7f67134909582f2
cjgmj <cjgmj@hotmail.com>
2019-08-08 17:26:53 +0200

    PagingAndSortingRepository

--- spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/dao/IClienteDao.java
@@ -1,9 +1,9 @@
 package com.cjgmj.datajpa.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.cjgmj.datajpa.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long> {
 
 }
--- spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/service/IClienteService.java
+++ spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/service/IClienteService.java
@@ -2,11 +2,16 @@ package com.cjgmj.datajpa.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.cjgmj.datajpa.entity.Cliente;
 
 public interface IClienteService {
 
 	public List<Cliente> findAll();
+
+	public Page<Cliente> findAll(Pageable pageable);
 
 	public Cliente findOne(Long id);
 
