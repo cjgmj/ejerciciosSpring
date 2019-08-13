15b9ed24d17e3fb3d2cc8033326ef74bf9260011
cjgmj <cjgmj@hotmail.com>
2019-08-13 16:58:54 +0200

    Formulario factura

---
+++ spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/controller/FacturaController.java
@@ -0,0 +1,42 @@
+package com.cjgmj.datajpa.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.cjgmj.datajpa.entity.Cliente;
+import com.cjgmj.datajpa.entity.Factura;
+import com.cjgmj.datajpa.service.IClienteService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+
+	@Autowired
+	private IClienteService clienteService;
+
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable Long clienteId, Map<String, Object> model, RedirectAttributes flash) {
+		Cliente cliente = clienteService.findOne(clienteId);
+		if (cliente == null) {
+			flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+			return "redirect:/listar";
+		}
+
+		Factura factura = new Factura();
+		factura.setCliente(cliente);
+
+		model.put("titulo", "Crear Factura");
+		model.put("factura", factura);
+
+		return "factura/form";
+	}
+
+}
