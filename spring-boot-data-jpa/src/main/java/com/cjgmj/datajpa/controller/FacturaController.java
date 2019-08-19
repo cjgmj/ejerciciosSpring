adc5ddf191b770d40fdb9f19f2f283d6206d4f2d
cjgmj <cjgmj@hotmail.com>
2019-08-19 17:48:16 +0200

    Eliminar factura

--- spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/controller/FacturaController.java
+++ spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/controller/FacturaController.java
@@ -111,4 +111,18 @@ public class FacturaController {
 		return "redirect:/ver/" + factura.getCliente().getId();
 	}
 
+	@GetMapping("/eliminar/{id}")
+	public String eliminar(@PathVariable Long id, RedirectAttributes flash) {
+		Factura factura = clienteService.findFacturaById(id);
+
+		if (factura != null) {
+			clienteService.deleteFactura(id);
+			flash.addFlashAttribute("success", "Factura eliminada con éxito");
+			return "redirect:/ver/" + factura.getCliente().getId();
+		}
+
+		flash.addFlashAttribute("error", "La factura no existe");
+		return "redirect:/listar";
+	}
+
 }
