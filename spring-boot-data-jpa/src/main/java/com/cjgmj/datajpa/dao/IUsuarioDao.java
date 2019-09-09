2a3d49a771709d740dadd0ad62be864511247481
cjgmj <cjgmj@hotmail.com>
2019-09-09 21:07:08 +0200

    Repositorio Usuario

---
+++ spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/dao/IUsuarioDao.java
@@ -0,0 +1,9 @@
+package com.cjgmj.datajpa.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cjgmj.datajpa.entity.Usuario;
+
+public interface IUsuarioDao extends CrudRepository<Usuario, Long> {
+
+}
