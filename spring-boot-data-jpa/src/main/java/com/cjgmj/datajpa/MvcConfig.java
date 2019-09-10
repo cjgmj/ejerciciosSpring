a2f60638596af31e0f88fdee6f2e5c85e4383258
cjgmj <cjgmj@hotmail.com>
2019-09-10 17:16:10 +0200

    Configurar multilenguaje

--- spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/cjgmj/datajpa/MvcConfig.java
@@ -1,10 +1,16 @@
 package com.cjgmj.datajpa;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -17,6 +23,27 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localChangeInterceptor() {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+
+		return localeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localChangeInterceptor());
 	}
 
 }
