2b7cd8bec76e561c084d78a91745db32cdc7018b
cjgmj <cjgmj@hotmail.com>
2019-09-22 13:14:56 +0200

    Generar JWT

--- spring-boot-jwt/src/main/java/com/cjgmj/jwt/auth/filter/JWTAuthenticationFilter.java
+++ spring-boot-jwt/src/main/java/com/cjgmj/jwt/auth/filter/JWTAuthenticationFilter.java
@@ -1,5 +1,12 @@
 package com.cjgmj.jwt.auth.filter;
 
+import java.io.IOException;
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,12 +14,21 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
 	private AuthenticationManager authenticationManager;
+
+	public static final Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
 	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
 		this.authenticationManager = authenticationManager;
@@ -44,6 +60,25 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
 
 		return authenticationManager.authenticate(authToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		String username = ((User) authResult.getPrincipal()).getUsername();
+		String token = Jwts.builder().setSubject(username).signWith(SECRET_KEY).compact();
+
+		response.addHeader("Authorization", "Bearer " + token);
+
+		Map<String, Object> body = new HashMap<>();
+		body.put("token", token);
+		body.put("user", authResult.getPrincipal());
+		body.put("mensaje", String.format("Hola %s, has iniciado sesión con éxito", username));
+
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(200);
+		response.setContentType("application/json");
 	}
 
 }
