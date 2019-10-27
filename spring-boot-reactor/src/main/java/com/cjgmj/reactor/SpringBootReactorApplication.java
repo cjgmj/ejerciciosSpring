d5ed5d94db6b44a0b708978bb66e48e2362f7c40
cjgmj <cjgmj@hotmail.com>
2019-10-27 11:23:21 +0100

    Operador map

--- spring-boot-reactor/src/main/java/com/cjgmj/reactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/cjgmj/reactor/SpringBootReactorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cjgmj.reactor.models.Usuario;
+
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
@@ -19,16 +21,20 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Joe", "John", "Linus").doOnNext(e -> {
-			if (e.isEmpty()) {
-				throw new RuntimeException("El nombre no puede estar vacío");
-			}
+		Flux<Usuario> nombres = Flux.just("Joe", "John", "Linus").map(nombre -> new Usuario(nombre.toUpperCase(), null))
+				.doOnNext(usuario -> {
+					if (usuario == null) {
+						throw new RuntimeException("El nombre no puede estar vacío");
+					}
 
-			System.out.println(e);
-		});
+					System.out.println(usuario.getNombre());
+				}).map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
+				});
 
-		nombres.subscribe(e -> LOG.info(e), error -> LOG.error(error.getMessage()), new Runnable() {
-
+		nombres.subscribe(e -> LOG.info(e.toString()), error -> LOG.error(error.getMessage()), new Runnable() {
 			@Override
 			public void run() {
 				LOG.info("Se ha completado la ejecución del observable");
