8b4f5a18df16b1576f0bd70b9a8ffc80599b976d
cjgmj <cjgmj@hotmail.com>
2019-11-01 14:06:43 +0100

    Cambiar base de datos y añadir fecha de creación

--- spring-boot-webflux/src/main/java/com/cjgmj/webflux/SpringBootWebfluxApplication.java
+++ spring-boot-webflux/src/main/java/com/cjgmj/webflux/SpringBootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package com.cjgmj.webflux;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,9 +38,10 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 				new Producto("Apple iPod", 46.89), new Producto("Sony Notebook", 846.89),
 				new Producto("Hewlett Packard Multifuncional", 200.89), new Producto("Bianchi Bicicleta", 70.89),
 				new Producto("HP Notebook Omen 17", 2500.89), new Producto("Mica Cómoda 5 cajones", 150.89),
-				new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89))
-				.flatMap(producto -> productoDao.save(producto))
-				.subscribe(producto -> LOG.info("Insert: " + producto.getId() + " " + producto.getNombre()));
+				new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89)).flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return productoDao.save(producto);
+				}).subscribe(producto -> LOG.info("Insert: " + producto.getId() + " " + producto.getNombre()));
 	}
 
 }
