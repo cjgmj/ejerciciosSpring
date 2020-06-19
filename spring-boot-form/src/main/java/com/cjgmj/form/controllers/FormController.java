4ec0ef7af573e1bc7143815d9f3bf2286fbad0e9
cjgmj <cjgmj@hotmail.com>
2020-06-19 15:23:16 +0200

    Atributo object de Thymeleaf

--- spring-boot-form/src/main/java/com/cjgmj/form/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/cjgmj/form/controllers/FormController.java
@@ -1,8 +1,5 @@
 package com.cjgmj.form.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -35,13 +32,6 @@ public class FormController {
 		model.addAttribute("titulo", "Resultado form");
 
 		if (result.hasErrors()) {
-			final Map<String, String> errores = new HashMap<>();
-
-			result.getFieldErrors().forEach(err -> errores.put(err.getField(),
-					"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage())));
-
-			model.addAttribute("error", errores);
-
 			return "form";
 		}
 
