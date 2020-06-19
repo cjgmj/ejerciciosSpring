9d003c97bb9712d1eeed98d3dc78ac121aedaa12
cjgmj <cjgmj@hotmail.com>
2020-06-19 15:09:28 +0200

    Añadir mensajes de error en la vista

--- spring-boot-form/src/main/java/com/cjgmj/form/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/cjgmj/form/controllers/FormController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cjgmj.form.models.domain.Usuario;
@@ -19,6 +20,7 @@ public class FormController {
 	@GetMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("titulo", "Formulario usuarios");
+		model.addAttribute("usuario", new Usuario());
 
 		return "form";
 	}
@@ -26,15 +28,17 @@ public class FormController {
 	// BindingResult contiene los mensajes de error de la validación, en caso de
 	// haber errores, se inyecta de forma automática al aparecer la anotación
 	// @Valid. Debe estar justo después del objeto a validar
+	// @ModelAttribute indica el nombre con el que el atributo será pasado a la
+	// vista en caso de que falle la validación
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model) {
 		model.addAttribute("titulo", "Resultado form");
 
 		if (result.hasErrors()) {
 			final Map<String, String> errores = new HashMap<>();
 
 			result.getFieldErrors().forEach(err -> errores.put(err.getField(),
-					"El campo".concat(err.getField()).concat(" ").concat(err.getDefaultMessage())));
+					"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage())));
 
 			model.addAttribute("error", errores);
 
