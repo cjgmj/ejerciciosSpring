82fa798a49661eba0cd609be65b7b978011b1077
cjgmj <cjgmj@hotmail.com>
2020-06-19 16:13:31 +0200

    Anotación @SessionAttributes

--- spring-boot-form/src/main/java/com/cjgmj/form/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/cjgmj/form/controllers/FormController.java
@@ -8,10 +8,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.cjgmj.form.models.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 
 	@GetMapping("/form")
@@ -32,8 +35,11 @@ public class FormController {
 	// @Valid. Debe estar justo después del objeto a validar
 	// @ModelAttribute indica el nombre con el que el atributo será pasado a la
 	// vista en caso de que falle la validación
+	// SessionStatus se usa para completar la sesión y eliminar los objetos que
+	// estén en ella
 	@PostMapping("/form")
-	public String procesar(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid @ModelAttribute("usuario") Usuario usuario, BindingResult result, Model model,
+			SessionStatus status) {
 		model.addAttribute("titulo", "Resultado form");
 
 		if (result.hasErrors()) {
@@ -41,6 +47,8 @@ public class FormController {
 		}
 
 		model.addAttribute("usuario", usuario);
+
+		status.setComplete();
 
 		return "resultado";
 	}
