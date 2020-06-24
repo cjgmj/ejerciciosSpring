e01b2e66c535f91472d1287dc144d0459b3f12b4
cjgmj <cjgmj@hotmail.com>
2020-06-24 14:31:48 +0200

    Personalizar error 404 y 500

--- spring-boot-error/src/main/java/com/cjgmj/error/controllers/AppController.java
+++ spring-boot-error/src/main/java/com/cjgmj/error/controllers/AppController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
+	@SuppressWarnings("unused")
 	@GetMapping("/index")
 	public String index() {
+		final Integer valor = 100 / 0;
 		return "index";
 	}
 
