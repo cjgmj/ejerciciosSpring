dc1295223bf6e6a1d17e1f16f1097a975de7125c
cjgmj <cjgmj@hotmail.com>
2020-06-24 14:42:29 +0200

    Anotación @ControllerAdvice

---
+++ spring-boot-error/src/main/java/com/cjgmj/error/controllers/ErrorHandlerController.java
@@ -0,0 +1,26 @@
+package com.cjgmj.error.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+
+	// Puede capturar varia excepciones encapsulándolas entre llaves
+	// El tipo que se la pasa por argumentos puede ser el genérico o el tipo
+	// concreto
+	@ExceptionHandler(ArithmeticException.class)
+	public String aritmeticaError(ArithmeticException ex, Model model) {
+		model.addAttribute("error", "Error de aritmética");
+		model.addAttribute("message", ex.getMessage());
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		model.addAttribute("timestamp", new Date());
+
+		return "error/aritmetica";
+	}
+
+}
