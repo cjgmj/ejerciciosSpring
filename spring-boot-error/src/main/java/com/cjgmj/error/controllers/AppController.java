46eddf4f2ec5001901516d1358ee63383a716200
cjgmj <cjgmj@hotmail.com>
2020-06-24 15:05:58 +0200

    Método handler ver

--- spring-boot-error/src/main/java/com/cjgmj/error/controllers/AppController.java
+++ spring-boot-error/src/main/java/com/cjgmj/error/controllers/AppController.java
@@ -1,10 +1,19 @@
 package com.cjgmj.error.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cjgmj.error.models.Usuario;
+import com.cjgmj.error.service.UsuarioService;
 
 @Controller
 public class AppController {
+
+	@Autowired
+	private UsuarioService usuarioService;
 
 	@SuppressWarnings("unused")
 	@GetMapping("/index")
@@ -12,6 +21,16 @@ public class AppController {
 //		final Integer valor = 100 / 0;
 		final Integer valor = Integer.parseInt("10x");
 		return "index";
+	}
+
+	@GetMapping("/ver/{id}")
+	public String ver(@PathVariable Integer id, Model model) {
+		final Usuario usuario = this.usuarioService.obtenerPorId(id);
+
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Detalle usuario: ".concat(usuario.getNombre()));
+
+		return "ver";
 	}
 
 }
